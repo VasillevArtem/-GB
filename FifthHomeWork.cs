13dacb1c4fc7b6537422714d96ccad3172842ae0
VasillevArtem <94571976+VasillevArtem@users.noreply.github.com>
2022-11-06 21:16:08 -0500

    Create FifthHomeWork.cs

---
+++ FifthHomeWork.cs
@@ -0,0 +1,50 @@
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+static void SumOdd()
+{
+    Random random = new Random();
+    int[] array = new int[random.Next(10, 100)];
+    int sumOdd = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+        if (i % 2 != 0)
+        {
+            sumOdd += array[i];
+        }
+    }
+    PrintInConsole(sumOdd);
+}
+
+
+//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+static void SumBetweenMaxAndMin()
+{
+    Random random = new Random();
+    double[] array = new double[random.Next(10, 100)];
+    double sumOdd = 0;
+     
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = NextDouble(random);
+    }
+    sumOdd = array.Max() - array.Min();
+    PrintInConsole(sumOdd);
+
+}
+
+
+static double NextDouble(Random random)
+{
+    double mantissa = (random.NextDouble() * 2.0) - 1.0;    
+    double exponent = Math.Pow(2.0, random.Next(-126, 128));
+    return (mantissa * exponent);
+}
+
+
+//Метод для печати в консоль.
+static void PrintInConsole<T>(T value)
+{
+    System.Console.WriteLine(value);
+}
